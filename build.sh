0955e9c73212751a691de804bd9fdad9fc4bf964
Jort <jordan.jennings@mystenlabs.com>
2025-01-16 23:17:27 -0800

    change build script to wasm pack

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cargo build --target wasm32-unknown-unknown --release
-wasm-bindgen target/wasm32-unknown-unknown/release/mining-poc.wasm --out-dir ./out --no-typescript
+wasm-pack build --target web
